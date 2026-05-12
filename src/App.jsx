161716d270eaae61bb10b04d8f957e44f3dc303b
ZyroBad <zyrobadj@gmail.com>
2026-05-11 23:18:28 -0600

    main: proyecto base sin codigo de niveles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import Pomodoro from './Pomodoro'
-
 function App() {
   return (
     <div>
-      <Pomodoro />
+      <h1>React Hooks - Pomodoro Timer</h1>
+      <p>Selecciona una rama para ver cada nivel:</p>
+      <ul>
+        <li>nivel-1: Timer básico</li>
+        <li>nivel-2: Work/Break + Historial</li>
+        <li>nivel-3: Configuración + Estadísticas</li>
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
